61eeb52e80f531acd2a33366a50a0658b0b15d89
c3nsored <ur.gabriel@outlook.com>
2020-04-25 12:35:55 +0200

    Code review

--- records/auth.py
+++ records/auth.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, flash, render_template
 from flask_login import current_user, login_user
-
 from .forms import LoginForm
 from .models import User
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    #FIXME: 
     if current_user.is_authenticated:
         return redirect(url_for('login'))
     
@@ -16,15 +15,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
     
-        if user in None or not user.check_password(form.password.data):
+        if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('fail'))
+            return "Login failed!"
 
         login_user(user, remeber=form.remember_me.data)
-        return redirect(url_for('fail'))
+        return "Login failed!"
     
     return render_template('auth/login.html', title='Sign In', form=form)
 
 
-#TODO: Configure Admin login
-    #TODO: add logout 
